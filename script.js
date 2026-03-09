b7e6bde70a3e7d9472e3c0e3dc29b2716a972f9d
Ohiomata BN <benjaminohiomatanathaniel@gmail.com>
2026-03-09 09:26:03 +0100

    create the questions and answer HTML using createQuestionHTML and createAnswersHTML function
    and alos called the loardHintsAndAnswer function to render the questions and answers html

--- script.js
+++ script.js
@@ -10,4 +10,30 @@ function loadHintsAndAnswers() {
     questionBank.push([techQA[ranNum].hint, techQA[ranNum].id]);
     answersBank.push([techQA[ranNum].answer, techQA[ranNum].id]);
   }
+
+  createQuestionHTML();
+  createAnswersHTML();
 }
+
+function createQuestionHTML() {
+  let QuestionHTML = "";
+  let answerHTML = "";
+  questionBank.forEach((question) => {
+    QuestionHTML += `
+            <div class="box" data-id="${question[1]}">${question[0]}"</div>
+        `;
+  });
+  document.getElementById("questions-container").innerHTML = QuestionHTML;
+}
+
+function createAnswersHTML() {
+  let answerHTML = "";
+  questionBank.forEach((answer) => {
+    answerHTML += `
+            <div class="box" data-id="${answer[1]}">${answer[0]}"</div>
+        `;
+  });
+  document.getElementById("answers-container").innerHTML = answerHTML;
+}
+
+loadHintsAndAnswers();
